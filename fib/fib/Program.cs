8681604163e0b08f98ac8e022b2d7f1b2ecd40ad
Jelonidas <72608651+Jelonidas@users.noreply.github.com>
2024-01-21 15:46:36 +0100

    Add files via upload

---
+++ fib/fib/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(Fib(4)); // Powinno zwrócić 3
+    }
+
+    static int Fib(int n)
+    {
+        if (n == 0)
+        {
+            return 0;
+        }
+        else if (n == 1)
+        {
+            return 1;
+        }
+
+        int[] fibSeries = new int[n + 1];
+        fibSeries[0] = 0;
+        fibSeries[1] = 1;
+
+        for (int i = 2; i <= n; i++)
+        {
+            fibSeries[i] = fibSeries[i - 1] + fibSeries[i - 2];
+        }
+
+        return fibSeries[n];
+    }
+}
